c64d733175daadf19cd8b57f9df8d6a3a86f22a7
Shawna Summers <shawnasummers777@gmail.com>
2026-02-11 07:26:17 -0500

    fixed buttons to change back to original style when another button is clicked and the content in the first row of the table changes when button is clicked

--- courses.js
+++ courses.js
@@ -1,5 +1,44 @@
 function csc1210() {
     //change the content in the table
     document.getElementById('week1').innerHTML = "csc1210 week1 topics";
+    document.getElementById('csc1210').style.background = "lightblue";
+    document.getElementById('csc1210').style.height = '22px';
+    document.getElementById('csc1210').style.fontsize = 'large';  
+  
+    document.getElementById('csc1705').style.background = "white";
+    document.getElementById('csc1705').style.height = '18px';
+  
+    document.getElementById('csc3212').style.background = "white";
+    document.getElementById('csc3212').style.height = '18px';
+  
     
+}
+
+function csc1705() {
+    //change the content in the table
+    document.getElementById('week1').innerHTML = "csc1705 week1 topics";
+    document.getElementById('csc1705').style.background = "lightblue";
+    document.getElementById('csc1705').style.height = '22px';
+    document.getElementById('csc1705').style.fontsize = 'large';   
+  
+    document.getElementById('csc1210').style.background = "white";
+    document.getElementById('csc1210').style.height = '18px';
+  
+    document.getElementById('csc3212').style.background = "white";
+    document.getElementById('csc3212').style.height = '18px';
+ 
+}
+
+function csc3212() {
+    //change the content in the table
+    document.getElementById('week1').innerHTML = "csc3212 week1 topics";
+    document.getElementById('csc3212').style.background = "lightblue";
+    document.getElementById('csc3212').style.height = '22px';
+    document.getElementById('csc3212').style.fontsize = 'large';
+  
+    document.getElementById('csc1210').style.background = "white";
+    document.getElementById('csc1210').style.height = '18px';
+  
+    document.getElementById('csc1705').style.background = "white";
+    document.getElementById('csc1705').style.height = '18px';
 }
